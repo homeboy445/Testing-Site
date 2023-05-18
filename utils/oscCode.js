dcd052d7441f439d532ab182a45caee40efc4351
Akshit Kushwaha <akshit.kushwaha@wingify.com>
2023-05-18 11:59:13 +0530

    added some changes

---
+++ utils/oscCode.js
@@ -0,0 +1,7 @@
+(function() {
+    const script = document.createElement("script");
+    script.src = "https://dev.visualwebsiteoptimizer.com/lib/644092.js";
+    script.id = "vwoCode";
+    document.head.appendChild(script);
+})();
+
